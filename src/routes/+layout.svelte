d6972917aa0e51da84f5da7b03fde2f1114473af
Specy <specy.dev@gmail.com>
2023-02-13 12:00:44 +0100

    Added installable app

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,26 +1,40 @@
 <script lang="ts">
-	import Navbar from "$cmp/Navbar.svelte";
-import LoggerProvider from "$cmp/providers/LoggerProvider.svelte";
-	import PromptProvider from "$cmp/providers/PromptProvider.svelte";
-	import ThemeProvider from "$cmp/providers/ThemeProvider.svelte";
-    import '../global.css'
-</script>
+	import Navbar from '$cmp/Navbar.svelte';
+	import LoggerProvider from '$cmp/providers/LoggerProvider.svelte';
+	import PromptProvider from '$cmp/providers/PromptProvider.svelte';
+	import ThemeProvider from '$cmp/providers/ThemeProvider.svelte';
+	import { onMount } from 'svelte';
+	import '../global.css';
 
+	onMount(async () => {
+		if ('serviceWorker' in navigator) {
+			try {
+				const registration = await navigator.serviceWorker.register('/sw.js');
+				if (registration.installing) {
+					console.log('Service worker installing');
+				} else if (registration.waiting) {
+					console.log('Service worker installed');
+				} else if (registration.active) {
+					console.log('Service worker active');
+				}
+			} catch (error) {
+				console.error(`Registration failed with ${error}`);
+			}
+		}
+	});
+</script>
 
 <Navbar />
 <ThemeProvider>
-    <PromptProvider>
-        <LoggerProvider>
-            <slot />
-        </LoggerProvider>
-    </PromptProvider>
+	<PromptProvider>
+		<LoggerProvider>
+			<slot />
+		</LoggerProvider>
+	</PromptProvider>
 </ThemeProvider>
 
-
-
-
 <style lang="scss">
-    :global(body) {
-        background-color: var(--background);
-    }
+	:global(body) {
+		background-color: var(--background);
+	}
 </style>
